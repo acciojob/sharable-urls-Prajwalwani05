4921d2681f0acb76a5b5fa444c89951d2b52d60d
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-26 19:52:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+function url(){
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+	var url = "https://localhost:8080/?name=" + encodeURIComponent(name);
+
+      if (year) {
+        url += "&year=" + encodeURIComponent(year);
+      }
+
+      document.getElementById("url").textContent = url;
+}
